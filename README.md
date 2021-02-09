4b204efb707593213795679e6f3312ecb66889f5
Hauke Jung <hauke.jung@outlook.de>
2021-02-09 08:05:58 +0100

    Broken test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotnet-ci-cd
+
+![Continuous Integration and Deployment](https://github.com/hauju/dotnet-ci-cd/workflows/Continuous%20Integration%20and%20Deployment/badge.svg)
--- WebApp.Tests/UnitTest1.cs
+++ WebApp.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace WebApp.Tests
         [Fact]
         public void Test1()
         {
-            Assert.True(1 == 1);
+            Assert.True(1 == 0);
         }
     }
 }
